3ce650952b7d05b49518d8fa6ed114ba3618d61e
Saravana Kumar <saravanakumarforwork@gmail.com>
2024-06-21 17:02:17 +0530

    Create Scatterplot.jl

---
+++ Script/Scatterplot.jl
@@ -0,0 +1,15 @@
+#Required Libraries
+using Pkg
+Pkg.add("Plots")
+Pkg.add("GR")
+
+#Scatter Plot
+using Plots
+gr()  # Use the GR backend
+
+# Generate some data
+x = 1:10
+y = rand(10)
+
+# Create a scatter plot
+scatter(x, y, title="Scatter Plot", xlabel="X-axis", ylabel="Y-axis", label="Points", markersize=5)
