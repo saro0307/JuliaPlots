502119f3c424efbb1576eeb3a533199abd175e6d
Saravana Kumar <saravanakumarforwork@gmail.com>
2024-06-21 17:00:37 +0530

    Create lineplot.jl

---
+++ Script/lineplot.jl
@@ -0,0 +1,16 @@
+#Required Libraries
+using Pkg
+Pkg.add("Plots")
+Pkg.add("GR"); # GR is a popular backend for Plots.jl
+
+#Line Plot
+
+using Plots
+gr()  # Use the GR backend
+
+# Generate some data
+x = 1:10
+y = rand(10)
+
+# Create a line plot
+plot(x, y, title="Line Plot", xlabel="X-axis", ylabel="Y-axis", label="Line", lw=2)
