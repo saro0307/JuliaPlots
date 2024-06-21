63c9b3cfa606469644f10b9f7e59f285d1d8d549
Saravana Kumar <saravanakumarforwork@gmail.com>
2024-06-21 17:03:29 +0530

    Create Barplot.jl

---
+++ Script/Barplot.jl
@@ -0,0 +1,15 @@
+#Required Libraries
+using Pkg
+Pkg.add("Plots")
+Pkg.add("GR")
+
+#Bar plot
+using Plots
+gr()  # Use the GR backend
+
+# Generate some data
+categories = ["A", "B", "C", "D"]
+values = [5, 7, 3, 6]
+
+# Create a bar plot
+bar(categories, values, title="Bar Plot", xlabel="Categories", ylabel="Values", label="Bars")
